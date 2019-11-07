e68ea087660368486a40a34ff32c144bda2d5619
levi davis <levidavis@pursuit.org>
2019-11-07 12:26:30 -0500

    Adds map view and two search bars via programtic UI. Adds scene delegate code

--- unit-five-project-one/Controllers/MapViewController.swift
+++ unit-five-project-one/Controllers/MapViewController.swift
@@ -7,24 +7,78 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        
+        return mapView
+    }()
+    
+    lazy var venueSearchBar: UISearchBar = {
+        let venueSearchBar = UISearchBar()
+        venueSearchBar.placeholder = "Enter Venue"
+        return venueSearchBar
+    }()
+
+    lazy var locationSearchBar: UISearchBar = {
+        let locationSearchBar = UISearchBar()
+        locationSearchBar.placeholder = "Enter Location"
+        return locationSearchBar
+
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        addSubviews()
+        constrainSubviews()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+       
 }
+
+extension MapViewController {
+    private func addSubviews() {
+        view.addSubview(mapView)
+        view.addSubview(venueSearchBar)
+        view.addSubview(locationSearchBar)
+    }
+    
+    private func constrainSubviews() {
+        constrainMapView()
+        constrainVenueSearchBar()
+        constrainLocationSearchBar()
+    }
+    
+    private func constrainMapView() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        [mapView.topAnchor.constraint(equalTo: locationSearchBar.bottomAnchor),
+         mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+         mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+         mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)].forEach{$0.isActive = true}
+        
+    }
+    
+    private func constrainVenueSearchBar() {
+        venueSearchBar.translatesAutoresizingMaskIntoConstraints = false
+        [venueSearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+         venueSearchBar.heightAnchor.constraint(equalToConstant: 50),
+         venueSearchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+         venueSearchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)].forEach{$0.isActive = true}
+    }
+    
+    private func constrainLocationSearchBar() {
+        locationSearchBar.translatesAutoresizingMaskIntoConstraints = false
+        [locationSearchBar.topAnchor.constraint(equalTo: venueSearchBar.bottomAnchor),
+         locationSearchBar.heightAnchor.constraint(equalToConstant: 50),
+         locationSearchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+         locationSearchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)].forEach{$0.isActive = true}
+    }
+    
+    
+}
+
